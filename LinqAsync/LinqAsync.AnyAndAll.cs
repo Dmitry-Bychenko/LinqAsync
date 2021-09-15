1106be9d05d94d521d26b1b33da7ff8845d5dbfb
Dmitry Bychenko <Zwerg.CopperNickel@gmail.com>
2021-09-15 23:37:53 +0300

    Add project files.

---
+++ LinqAsync/LinqAsync.AnyAndAll.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqAsync {
+  
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// 
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class AsyncEnumerableExtensions {
+    #region Public 
+
+    /// <summary>
+    /// Any
+    /// </summary>
+    public static async Task<bool> AnyAsync<T>(this IAsyncEnumerable<T> source, 
+                                                    Func<T, bool> predicate,
+                                                    CancellationToken token) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+      if (predicate is null)
+        throw new ArgumentNullException(nameof(predicate));
+
+      token.ThrowIfCancellationRequested();
+
+      await foreach (var item in source.ConfigureAwait(false)) {
+        token.ThrowIfCancellationRequested();
+
+        if (predicate(item))
+          return true;
+      }
+
+      return false;
+    }
+
+    /// <summary>
+    /// Any
+    /// </summary>
+    public static async Task<bool> AnyAsync<T>(this IAsyncEnumerable<T> source,
+                                                    Func<T, bool> predicate) =>
+      await AnyAsync(source, predicate, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Any
+    /// </summary>
+    public static async Task<bool> AnyAsync<T>(this IAsyncEnumerable<T> source,
+                                                    CancellationToken token) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+
+      token.ThrowIfCancellationRequested();
+
+      await foreach (var item in source.ConfigureAwait(false)) {
+        token.ThrowIfCancellationRequested();
+
+        return true;
+      }
+
+      return false;
+    }
+
+    /// <summary>
+    /// Any
+    /// </summary>
+    public static async Task<bool> AnyAsync<T>(this IAsyncEnumerable<T> source) =>
+      await AnyAsync(source, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// All
+    /// </summary>
+    public static async Task<bool> AllAsync<T>(this IAsyncEnumerable<T> source, 
+                                                    Func<T, bool> predicate,
+                                                    CancellationToken token) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+      if (predicate is null)
+        throw new ArgumentNullException(nameof(predicate));
+
+      token.ThrowIfCancellationRequested();
+
+      await foreach (var item in source.ConfigureAwait(false)) {
+        token.ThrowIfCancellationRequested();
+
+        if (!predicate(item))
+          return false;
+      }
+
+      return true;
+    }
+
+    /// <summary>
+    /// All
+    /// </summary>
+    public static async Task<bool> AllAsync<T>(this IAsyncEnumerable<T> source,
+                                                    Func<T, bool> predicate) =>
+      await AllAsync(source, predicate, CancellationToken.None).ConfigureAwait(false);
+
+    #endregion Public
+  }
+
+}
---
+++ LinqAsync/LinqAsync.Count.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LinqAsync {
+
+  //-------------------------------------------------------------------------------------------------------------------
+  //
+  /// <summary>
+  /// 
+  /// </summary>
+  //
+  //-------------------------------------------------------------------------------------------------------------------
+
+  public static partial class AsyncEnumerableExtensions {
+    #region Public 
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<int> CountAsync<T>(this IAsyncEnumerable<T> source, 
+                                                     Func<T, bool> predicate, 
+                                                     CancellationToken token) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+      if (predicate is null)
+        throw new ArgumentNullException(nameof(predicate));
+
+      token.ThrowIfCancellationRequested();
+
+      int count = 0;
+
+      await foreach (var item in source.ConfigureAwait(false)) {
+        token.ThrowIfCancellationRequested();
+
+        if (predicate(item))
+          Interlocked.Increment(ref count);
+      }
+
+      return count;
+    }
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<int> CountAsync<T>(this IAsyncEnumerable<T> source,
+                                                     Func<T, bool> predicate) =>
+      await CountAsync(source, predicate, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<int> CountAsync<T>(this IAsyncEnumerable<T> source,
+                                                     CancellationToken token) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+
+      token.ThrowIfCancellationRequested();
+
+      int count = 0;
+
+      await foreach (var item in source.ConfigureAwait(false)) {
+        token.ThrowIfCancellationRequested();
+
+        Interlocked.Increment(ref count);
+      }
+
+      return count;
+    }
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<int> CountAsync<T>(this IAsyncEnumerable<T> source) =>
+      await CountAsync(source, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<long> CountLongAsync<T>(this IAsyncEnumerable<T> source, 
+                                                          Func<T, bool> predicate,
+                                                          CancellationToken token) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+      if (predicate is null)
+        throw new ArgumentNullException(nameof(predicate));
+
+      token.ThrowIfCancellationRequested();
+
+      long count = 0;
+
+      await foreach (var item in source.ConfigureAwait(false)) {
+        token.ThrowIfCancellationRequested();
+
+        if (predicate(item))
+          Interlocked.Increment(ref count);
+      }
+
+      return count;
+    }
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<long> CountLongAsync<T>(this IAsyncEnumerable<T> source,
+                                                          Func<T, bool> predicate) =>
+      await CountLongAsync(source, predicate, CancellationToken.None).ConfigureAwait(false);
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<long> CountLongAsync<T>(this IAsyncEnumerable<T> source, 
+                                                          CancellationToken token) {
+      if (source is null)
+        throw new ArgumentNullException(nameof(source));
+
+      token.ThrowIfCancellationRequested();
+
+      long count = 0;
+
+      await foreach (var item in source.ConfigureAwait(false)) {
+        token.ThrowIfCancellationRequested();
+
+        Interlocked.Increment(ref count);
+      }
+
+      return count;
+    }
+
+    /// <summary>
+    /// Count
+    /// </summary>
+    public static async Task<long> CountLongAsync<T>(this IAsyncEnumerable<T> source) =>
+      await CountLongAsync(source, CancellationToken.None).ConfigureAwait(false);
+
+    #endregion Public
+  }
+
+}
